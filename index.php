f611e03b1fc6a6d2e5ef6e2cea377a07424b8306
m15obayasi <m15obayasi@gmail.com>
2024-12-08 14:08:06 +0900

    初回コミット

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題７</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cherry+Swash:700" rel="stylesheet">
+</head>
+
+<body>
+    <header>Epoch Sort</header>
+    <div class="upperHalf">
+        <div class="upper">a</div>
+    </div>
+
+    <div class="lowerHalf">b</div>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</body>
+
+</html>
